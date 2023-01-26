11066ca215413dedcb1391efb62f9bcce48824dd
Óscar O <oscarforjuela@hotmail.com>
2023-01-26 10:29:03 -0500

    Nuevos cambios

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ function App() {
   return (
     <div className='todo-todo'>
       <Header></Header>
-      <h1>To Do List</h1>
+      <h1 className="titulo">To Do List</h1>
 
       <div className="todo-app">
 
